9fd7438bdf5a870a15d8b40ce4c3ef75c7a305f7
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-02-10 15:17:11 +1100

    init

---
+++ Amatino/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Amatino
+//
+//  Created by Hugh Jeremy on 10/2/18.
+//  Copyright © 2018 Amatino Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Amatino/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Amatino
+//
+//  Created by Hugh Jeremy on 10/2/18.
+//  Copyright © 2018 Amatino Pty Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
